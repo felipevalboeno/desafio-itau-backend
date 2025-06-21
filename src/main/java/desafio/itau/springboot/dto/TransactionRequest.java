1458831dcb9cdeba937bc24750476ef00bcca1ce
Felipe Valboeno <feliperocha14@hotmail.com>
2025-06-21 12:15:19 -0300

    Implementando Controller

--- src/main/java/desafio/itau/springboot/dto/TransactionRequest.java
+++ src/main/java/desafio/itau/springboot/dto/TransactionRequest.java
@@ -2,13 +2,12 @@ package desafio.itau.springboot.dto;
 
 import java.time.OffsetDateTime;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public class TransactionRequest {
 
     @NotNull
-    @Min(0)
+   
     private Double valor;
 
     @NotNull
