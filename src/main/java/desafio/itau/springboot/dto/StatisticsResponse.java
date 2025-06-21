3c908e47d3dcec2226fc698f87720947532a89f4
Felipe Valboeno <feliperocha14@hotmail.com>
2025-06-21 11:33:01 -0300

    Projeto criado - Classes: Model e DTO

---
+++ src/main/java/desafio/itau/springboot/dto/StatisticsResponse.java
@@ -0,0 +1,22 @@
+package desafio.itau.springboot.dto;
+
+import java.util.DoubleSummaryStatistics;
+
+public class StatisticsResponse {
+
+    private long count;
+    private double sum;
+    private double avg;
+    private double min;
+    private double max;
+    
+
+    public StatisticsResponse(DoubleSummaryStatistics stats){
+        this.count = stats.getCount();
+        this.sum = stats.getSum();
+        this.avg = stats.getAverage();
+        this.min = stats.getMin();
+        this.max = stats.getMax();
+
+    }
+}
